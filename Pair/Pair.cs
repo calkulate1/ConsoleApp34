5c7f1733681933e98196fb4cdd4e69b9aa7bbca9
AntonSvirid <adsvirid@tut.by>
2024-10-30 18:52:32 +0300

    Добавьте файлы проекта.

---
+++ Pair/Pair.cs
@@ -0,0 +1,22 @@
+﻿namespace PairClass
+{
+    public class Pair<TKey, TValue>
+    {
+        private TKey _key;
+        private TValue _value;
+
+        public TKey Key
+        {
+            get { return _key; }
+        }
+        public TValue Value
+        {
+            get { return _value; }
+        }
+        public Pair(TKey key, TValue value)
+        {
+            _key = key;
+            _value = value;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using PairClass;
+using System;
+
+namespace ConsoleApp34
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pair<string, int> nameAgePair = new Pair<string, int>("Alice", 30);
+            Console.WriteLine($"name: {nameAgePair.Key}, age: {nameAgePair.Value}");
+        }
+    }
+}
